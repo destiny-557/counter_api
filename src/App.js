e61b2ce7c00435b7869e458a9f98c3045f3be609
destiny-557 <danielle.sherrod@umbc.edu>
2021-10-16 01:27:43 -0400

    Add API call in 3 ways

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import './App.css';
 import countapi from 'countapi-js';
 
-function getCountApi() {
+
+//Gets Count of site visits from Daniellescounters
+function getDanielleCount() {
+  fetch('https://api.countapi.xyz/hit/daniellescounters/1ccb732e-b55a-4404-ad3f-0f99c02fe44', { method: 'GET' })
+  .then (response => response.json())
+  .then (data => alert('This site has been visited '+(data.value)+' times!')
+  )
+}
+
+//Gets Count of site visits for Danielles counters using xhr 
+function xhrCount() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", 'https://api.countapi.xyz/hit/daniellescounters/1ccb732e-b55a-4404-ad3f-0f99c02fe44');
+  xhr.responseType = "json";
+  xhr.onload = function() {
+    alert(`Danielle's Counters has been visited ${this.response.value} times!`);
+  }
+  xhr.send();
+}
+
+//Gets Count of site visits for all of CountApi 
+function countApijs() {
   countapi.visits().then((result) => {
-    alert('This button has been clicked '+(result.value)+' times!');
+    console.log(result.value);
+    alert(`Countapi.xyz has been visited ${result.value} times!`);
   });
 }
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getCountApi}>Check Current Hits Count</button>
+        <button onClick={getDanielleCount}>Total API Calls for Danielle's Counters (Fetch Method)</button>       
+        <button onClick={xhrCount}>Total API Calls for Danielle's Counters (XHR Method)</button>
+        <button onClick={countApijs}>Total API Calls for Countapi.xyz using CountApi-js </button>
       </header>
-    </div>
-    
+    </div>   
   );
 }
-
-
 
 export default App;
