c86da86a13ba515497aac8c55febb509101b8425
destiny-557 <danielle.sherrod@umbc.edu>
2021-10-15 22:41:45 -0400

    install counter module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import countapi from 'countapi-js';
+
+
+countapi.visits().then((result) => {
+  console.log(result.value);
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Current Hits Count</button>
       </header>
+      <p id="visits">... </p>
     </div>
+    
   );
 }
+
+
 
 export default App;
